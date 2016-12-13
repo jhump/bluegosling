cf77b5c63f19aee1fcfa485ee2156db7a58442aa
Josh Humphries <jh@fullstory.com>
2016-12-13 18:20:03 -0500

    javadoc fixes

--- src/com/bluegosling/collections/bits/package-info.java
+++ src/com/bluegosling/collections/bits/package-info.java
@@ -1,6 +1,8 @@
 /**
- * This package contains an alternative to {@link java.util.BitSet} that provides an immutable
- * view of a sequence of bits and includes mechanisms to navigate the sequence in arbitrarily-sized
- * chunks of bits.
+ * This package contains an alternative to the standard {@link java.util.BitSet} that provides an
+ * immutable view of a sequence of bits and includes mechanisms to navigate the sequence in
+ * arbitrarily-sized chunks of bits.
+ * 
+ * @see com.bluegosling.collections.bits.BitSequence
  */
 package com.bluegosling.collections.bits;
--- src/com/bluegosling/collections/queues/package-info.java
+++ src/com/bluegosling/collections/queues/package-info.java
@@ -1,5 +1,11 @@
 /**
  * New interfaces and implementations related to the standard Java {@link java.util.Queue}
  * interface.
+ * 
+ * <p>This package also includes a new related-but-different interface named
+ * {@link com.bluegosling.collections.queues.PriorityQueue}. This interface is quite different from
+ * the JRE's class of the same name in that it exposes additional operations of a classical
+ * priority-queue ADT, mainly {@code reduce-key}. This decouples an element's priority from its
+ * intrinsic value, making it more useful for certain types of graph algorithms.  
  */
 package com.bluegosling.collections.queues;
