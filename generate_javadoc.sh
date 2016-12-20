8468296c17353577139db0efe966a94ffc8c69d2
Josh Humphries <jh@fullstory.com>
2016-12-20 09:02:05 -0500

    re-generated javadoc, 12-20-16

--- generate_javadoc.sh
+++ generate_javadoc.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Finds all com.bluegosling.* packages (and sub-packages thereof) and generates javadoc for them.
-rm -rf javadoc/ 2> /dev/null
-mkdir javadoc/
+rm -rf docs/javadoc/ 2> /dev/null
+mkdir docs/javadoc/
 find src -type d \
 	| grep com/bluegosling/ \
 	| sed s/\\//\\./g | sed s/src\\.// \
 	| xargs javadoc -source 8 \
 	-classpath bin/:lib/commons-collections-testframework-3.2.1.jar:lib/javax.inject.jar:lib/junit-4.10.jar:lib/guava-19.0.jar:lib/javaparser-core-2.4.0.jar \
-	-d javadoc/ -sourcepath src/ -notimestamp \
+	-d docs/javadoc/ -sourcepath src/ -notimestamp \
 	-link https://docs.oracle.com/javase/8/docs/api/ \
 	-link http://docs.guava-libraries.googlecode.com/git/javadoc \
 	-overview src/com/bluegosling/overview.html
